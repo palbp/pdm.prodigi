8a023fa5e1d7c119b6e9782d9107aec5319048a7
Paulo Pereira <paulo.pereira@isel.pt>
2025-09-17 18:23:50 +0100

    Adds simplified Hello Android project for video M03

---
+++ demos/M03/HelloAndroid/app/src/main/java/prodigi/pdm/demos/helloandroid/MainActivity.kt
@@ -0,0 +1,30 @@
+package prodigi.pdm.demos.helloandroid
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import prodigi.pdm.demos.helloandroid.ui.theme.HelloAndroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HelloAndroidTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Text(
+                        text = stringResource(id = R.string.greeting),
+                        modifier = Modifier.padding(paddingValues = innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
